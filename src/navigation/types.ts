abe30f529cf99a9b2c7bd073c691866e944991b1
leosampc <leonardosamp11@gmail.com>
2023-03-02 14:38:25 -0300

    fix: change id from MovieDetails to number type

--- src/navigation/types.ts
+++ src/navigation/types.ts
@@ -2,7 +2,7 @@ import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 
 export type RootStackParamList = {
 	Home: undefined;
-	MovieDetails: { id: string };
+	MovieDetails: { id: number };
 };
 
 export type RootStackScreenProps<T extends keyof RootStackParamList> =
