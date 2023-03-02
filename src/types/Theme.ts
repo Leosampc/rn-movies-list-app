01af9851b91b2356159b4e4ac2ab6f6d4dee2bd8
leosampc <leonardosamp11@gmail.com>
2023-03-02 15:48:14 -0300

    refactor: change theme colors type to own file

---
+++ src/types/Theme.ts
@@ -0,0 +1,10 @@
+export interface IColors {
+	white: string;
+	black: string;
+	primary: string;
+	secondary: string;
+	tertiary: string;
+	purplePrimary: string;
+	purpleSecondary: string;
+	grayPrimary: string;
+}
--- src/types/styled.d.ts
+++ src/types/styled.d.ts
@@ -1,14 +1,7 @@
+import { IColors } from './Theme';
+
 declare module 'styled-components/native' {
 	export interface DefaultTheme {
-		colors: {
-			white: string;
-			black: string;
-			primary: string;
-			secondary: string;
-			tertiary: string;
-			purplePrimary: string;
-			purpleSecondary: string;
-			grayPrimary: string;
-		};
+		colors: IColors;
 	}
 }
