667640c19fc58f252a8853eab2799816b44cb637
leosampc <leonardosamp11@gmail.com>
2023-03-02 14:38:34 -0300

    feat: initial navigation and data fetching

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,45 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { moviesAPI } from '@/services';
+import { RootStackParamList } from '@/navigation/types';
+import { IMovie } from '@/types';
 
-function Home(): JSX.Element {
+type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
+
+function Home({ navigation }: Props): JSX.Element {
+	const [upComingMovies, setUpComingMovies] = useState<IMovie[]>([]);
+	const [currentPage, setCurrentPage] = useState<number>(1);
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+
+	const handleFetchUpComingMovies = useCallback(async () => {
+		setIsLoading(true);
+		try {
+			const response = await moviesAPI.getUpcoming(currentPage);
+			setUpComingMovies(response);
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setIsLoading(false);
+		}
+	}, [currentPage]);
+
+	const handleNavigateToDetails = (movieId: number) =>
+		navigation.navigate('MovieDetails', { id: movieId });
+
+	useEffect(() => {
+		handleFetchUpComingMovies();
+	}, [handleFetchUpComingMovies]);
 	return (
 		<View>
-			<Text>Hello World!</Text>
+			{upComingMovies.map((movie) => (
+				<TouchableOpacity
+					key={movie.id}
+					onPress={() => handleNavigateToDetails(movie.id)}
+				>
+					<Text>{movie.title}</Text>
+				</TouchableOpacity>
+			))}
 		</View>
 	);
 }
--- src/screens/MovieDetails/index.tsx
+++ src/screens/MovieDetails/index.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Text, View } from 'react-native';
+import { moviesAPI } from '@/services';
+import { IMovie } from '@/types';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { RootStackParamList } from '@/navigation/types';
 
-function MovieDetails(): JSX.Element {
-	return (
-		<View>
-			<Text>Hello World!</Text>
-		</View>
-	);
+type Props = NativeStackScreenProps<RootStackParamList, 'MovieDetails'>;
+
+function MovieDetails({ route }: Props): JSX.Element {
+	const { id } = route.params;
+	const [movie, setMovie] = useState<IMovie>();
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+
+	const handleFetchMovieDetails = useCallback(async () => {
+		setIsLoading(true);
+		try {
+			const response = await moviesAPI.getDetails(id);
+			setMovie(response);
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setIsLoading(false);
+		}
+	}, [id]);
+
+	useEffect(() => {
+		handleFetchMovieDetails();
+	}, [handleFetchMovieDetails]);
+	return <View>{!!movie && <Text>{JSON.stringify(movie)}</Text>}</View>;
 }
 
 export default MovieDetails;
