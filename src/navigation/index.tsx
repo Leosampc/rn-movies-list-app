8a164264ae34740e33f8d56b084e4aee52ccc8b5
leosampc <leonardosamp11@gmail.com>
2023-03-03 09:21:24 -0300

    feat: Home header style

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useTheme } from 'styled-components/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { RootStackParamList } from './types';
 import { Home, MovieDetails } from '@/screens';
@@ -6,9 +7,18 @@ import { Home, MovieDetails } from '@/screens';
 const Root = createNativeStackNavigator<RootStackParamList>();
 
 function RootNavigator() {
+	const theme = useTheme();
 	return (
 		<Root.Navigator>
-			<Root.Screen name="Home" component={Home} />
+			<Root.Screen
+				name="Home"
+				component={Home}
+				options={{
+					headerLargeTitle: true,
+					title: 'Upcoming Movies',
+					headerTintColor: theme.colors.secondary,
+				}}
+			/>
 			<Root.Screen name="MovieDetails" component={MovieDetails} />
 		</Root.Navigator>
 	);
