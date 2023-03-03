42e54feaf5b9c60a0c888a4be91e99561878a76a
leosampc <leonardosamp11@gmail.com>
2023-03-02 21:19:13 -0300

    feat: Text component

---
+++ src/components/general/Text/index.ts
@@ -0,0 +1,25 @@
+import { TextProps } from 'react-native/types';
+import styled from 'styled-components/native';
+import { IColors } from '@/types';
+
+interface Props extends TextProps {
+	color: keyof IColors;
+	fontWeight?:
+		| 'normal'
+		| 'bold'
+		| '100'
+		| '200'
+		| '300'
+		| '400'
+		| '500'
+		| '600'
+		| '700'
+		| '800'
+		| '900'
+		| undefined;
+}
+
+export default styled.Text<Props>`
+	color: ${({ theme, color }) => theme.colors[color] || theme.colors.black};
+	font-weight: ${({ fontWeight }) => fontWeight ?? 'normal'};
+`;
---
+++ src/components/general/index.ts
@@ -0,0 +1,2 @@
+export { default as Skeleton } from './Skeleton';
+export { default as Text } from './Text';
