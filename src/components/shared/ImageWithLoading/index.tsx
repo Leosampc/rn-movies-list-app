51e6e1cc6f1085c59e1134bb7f564ce3d29aba72
leosampc <leonardosamp11@gmail.com>
2023-03-03 09:21:08 -0300

    feat: added radius prop

--- src/components/shared/ImageWithLoading/index.tsx
+++ src/components/shared/ImageWithLoading/index.tsx
@@ -5,12 +5,14 @@ import { Skeleton } from '@/components';
 interface Props extends ImageProps {
 	width: number;
 	height: number;
+	radius?: number;
 }
 
 function ImageWithLoading({
 	style,
 	width,
 	height,
+	radius,
 	...props
 }: Props): JSX.Element {
 	const [loaded, setLoaded] = useState(false);
@@ -35,10 +37,19 @@ function ImageWithLoading({
 				},
 			]}
 		>
-			{!loaded && <Skeleton width={width} height={height} />}
+			{!loaded && <Skeleton width={width} height={height} radius={radius} />}
 			<Animated.Image
 				{...props}
-				style={[style, { position: 'absolute', opacity, width, height }]}
+				style={[
+					style,
+					{
+						position: 'absolute',
+						opacity,
+						width,
+						height,
+						borderRadius: radius,
+					},
+				]}
 				onLoad={onLoad}
 			/>
 		</View>
