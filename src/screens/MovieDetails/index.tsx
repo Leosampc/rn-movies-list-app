e4e467dd65ec6cb6f04b5338d2c92c3ad26bedcb
leosampc <leonardosamp11@gmail.com>
2023-03-03 18:53:26 -0300

    feat: InfoDetails screen

--- src/screens/MovieDetails/index.tsx
+++ src/screens/MovieDetails/index.tsx
@@ -1,14 +1,20 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { Text, View } from 'react-native';
-import { moviesAPI } from '@/services';
-import { IMovie } from '@/types';
+import { StatusBar, useWindowDimensions } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { moviesAPI } from '@/services';
+import { toHoursAndMinutes, formatDate } from '@/utils';
+import { GenreType, IMovie } from '@/types';
 import { RootStackParamList } from '@/navigation/types';
+import { ImageWithLoading, Badge } from '@/components';
+import { Info } from './components';
+import * as Styled from './styled';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'MovieDetails'>;
 
 function MovieDetails({ route }: Props): JSX.Element {
 	const { id } = route.params;
+	const { width } = useWindowDimensions();
+
 	const [movie, setMovie] = useState<IMovie>();
 	const [isLoading, setIsLoading] = useState<boolean>(false);
 
@@ -24,10 +30,62 @@ function MovieDetails({ route }: Props): JSX.Element {
 		}
 	}, [id]);
 
+	const renderGenres = (genres: GenreType[]) => (
+		<Styled.Row>
+			{genres.map((genre) => (
+				<Badge key={genre.id} title={genre.name} color="yellowPrimary" />
+			))}
+		</Styled.Row>
+	);
+
 	useEffect(() => {
 		handleFetchMovieDetails();
 	}, [handleFetchMovieDetails]);
-	return <View>{!!movie && <Text>{JSON.stringify(movie)}</Text>}</View>;
+
+	if (!movie) return <Styled.Screen></Styled.Screen>;
+
+	return (
+		<Styled.Screen>
+			<StatusBar barStyle="light-content" />
+			<ImageWithLoading
+				width={width}
+				height={width * 0.56}
+				source={{
+					uri: `https://image.tmdb.org/t/p/w500/${movie.backdropPath}`,
+				}}
+			/>
+			<Styled.Body>
+				<Styled.Title fontWeight="600">{movie.title}</Styled.Title>
+				<Styled.Row>
+					<Styled.VoteIcon
+						type="fontAwesome"
+						name="star"
+						size={18}
+						color="yellowPrimary"
+					/>
+					<Styled.VoteText>{`${movie.voteAverage.toFixed(
+						1
+					)}/10`}</Styled.VoteText>
+				</Styled.Row>
+				{movie.genres && renderGenres(movie.genres)}
+				<Styled.Row>
+					<Info
+						title="Length"
+						description={toHoursAndMinutes(movie.length ?? 0)}
+					/>
+					<Info title="Status" description={movie.status} />
+					<Info
+						title="Release Date"
+						description={formatDate(movie.releaseDate)}
+					/>
+				</Styled.Row>
+				<Styled.SubTitle fontWeight="700" color="secondary">
+					Description
+				</Styled.SubTitle>
+				<Styled.Overview>{movie.overview}</Styled.Overview>
+			</Styled.Body>
+		</Styled.Screen>
+	);
 }
 
 export default MovieDetails;
---
+++ src/screens/MovieDetails/styled.ts
@@ -0,0 +1,57 @@
+import { Dimensions } from 'react-native';
+import styled from 'styled-components/native';
+import { Text, VectorIcon } from '@/components';
+
+const { width } = Dimensions.get('window');
+
+export const Screen = styled.ScrollView`
+	flex: 1;
+	background-color: ${({ theme }) => theme.colors.white};
+`;
+
+const padding = (width - width * 0.9) / 2;
+export const Body = styled.View`
+	flex: 1;
+	padding: ${padding}px;
+	padding-top: ${padding + 10}px;
+	margin-top: -15px;
+	border-top-left-radius: 20px;
+	border-top-right-radius: 20px;
+	background-color: ${({ theme }) => theme.colors.white};
+`;
+
+export const Title = styled(Text)`
+	font-size: 22px;
+`;
+
+export const SubTitle = styled(Text)`
+	font-size: 18px;
+	margin: 12px 0px;
+`;
+
+export const Row = styled.View`
+	flex-direction: row;
+	flex-wrap: wrap;
+	align-items: center;
+	margin: 10px 0px;
+	gap: 10px;
+`;
+
+export const Separator = styled.View`
+	width: 8px;
+`;
+
+export const VoteText = styled(Text)`
+	font-size: 16px;
+	color: ${({ theme }) => theme.colors.grayPrimary};
+`;
+
+export const VoteIcon = styled(VectorIcon).attrs(() => ({
+	containerStyle: { marginRight: 10 },
+}))``;
+
+export const Overview = styled(Text)`
+	font-size: 14px;
+	line-height: 24px;
+	color: ${({ theme }) => theme.colors.grayPrimary};
+`;
