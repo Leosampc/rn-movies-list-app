156b677c21a293a038f8206ac283e165e2ad43d3
leosampc <leonardosamp11@gmail.com>
2023-03-06 10:27:08 -0300

    feat: move iconType and IconType to own file

---
+++ src/components/general/VectorIcon/iconType.ts
@@ -0,0 +1,44 @@
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Entypo from 'react-native-vector-icons/Entypo';
+import EvilIcons from 'react-native-vector-icons/EvilIcons';
+import Feather from 'react-native-vector-icons/Feather';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Foundation from 'react-native-vector-icons/Foundation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
+import Octicons from 'react-native-vector-icons/Octicons';
+import Zocial from 'react-native-vector-icons/Zocial';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+
+export type IconType =
+	| 'antd'
+	| 'entypo'
+	| 'evil'
+	| 'feather'
+	| 'fontAwesome'
+	| 'foundation'
+	| 'ionicons'
+	| 'material'
+	| 'materialCommunity'
+	| 'simpleLine'
+	| 'octicons'
+	| 'zocial'
+	| 'fontisto';
+
+export default {
+	antd: AntDesign,
+	entypo: Entypo,
+	evil: EvilIcons,
+	feather: Feather,
+	fontAwesome: FontAwesome,
+	foundation: Foundation,
+	ionicons: Ionicons,
+	material: MaterialIcons,
+	materialCommunity: MaterialCommunityIcons,
+	simpleLine: SimpleLineIcons,
+	octicons: Octicons,
+	zocial: Zocial,
+	fontisto: Fontisto,
+};
--- src/components/general/VectorIcon/index.tsx
+++ src/components/general/VectorIcon/index.tsx
@@ -1,51 +1,8 @@
 import React from 'react';
 import { StyleProp, View, ViewStyle } from 'react-native';
 import { useTheme } from 'styled-components/native';
-import AntDesign from 'react-native-vector-icons/AntDesign';
-import Entypo from 'react-native-vector-icons/Entypo';
-import EvilIcons from 'react-native-vector-icons/EvilIcons';
-import Feather from 'react-native-vector-icons/Feather';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import Foundation from 'react-native-vector-icons/Foundation';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
-import Octicons from 'react-native-vector-icons/Octicons';
-import Zocial from 'react-native-vector-icons/Zocial';
-import Fontisto from 'react-native-vector-icons/Fontisto';
 import { IColors } from '@/types';
-
-type IconType =
-	| 'antd'
-	| 'entypo'
-	| 'evil'
-	| 'feather'
-	| 'fontAwesome'
-	| 'foundation'
-	| 'ionicons'
-	| 'material'
-	| 'materialCommunity'
-	| 'simpleLine'
-	| 'octicons'
-	| 'zocial'
-	| 'fontisto';
-
-const iconType = {
-	antd: AntDesign,
-	entypo: Entypo,
-	evil: EvilIcons,
-	feather: Feather,
-	fontAwesome: FontAwesome,
-	foundation: Foundation,
-	ionicons: Ionicons,
-	material: MaterialIcons,
-	materialCommunity: MaterialCommunityIcons,
-	simpleLine: SimpleLineIcons,
-	octicons: Octicons,
-	zocial: Zocial,
-	fontisto: Fontisto,
-};
+import iconType, { IconType } from './iconType';
 
 interface Props {
 	size: number;
