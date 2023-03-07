e2b24c55e19538247563acd5333f2bc645880e01
leosampc <leonardosamp11@gmail.com>
2023-03-07 11:06:24 -0300

    test: changed file name extension

--- e2e/jest.config.js
+++ e2e/jest.config.js
@@ -1,7 +1,16 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
+	preset: 'react-native',
+	globals: {
+		'ts-jest': {
+			tsconfig: './tsconfig.spec.json',
+		},
+	},
+	transform: {
+		'\\.tsx?$': 'ts-jest',
+	},
 	rootDir: '..',
-	testMatch: ['<rootDir>/e2e/**/*.test.js'],
+	testMatch: ['<rootDir>/e2e/**/*.spec.ts'],
 	testTimeout: 120000,
 	maxWorkers: 1,
 	globalSetup: 'detox/runners/jest/globalSetup',
